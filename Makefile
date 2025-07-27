055fd409eeabb31278796b25acf777de97918e06
CJ Harries <thecjharries@users.noreply.github.com>
2025-07-27 18:36:56 -0500

    Test using normal card sort process (#5)

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ CARGO ?= cargo
 .PHONY: test
 test:
 	$(CARGO) test -- --test-threads 1
+
+.PHONY: coverage
+coverage:
+	$(CARGO) tarpaulin -v --fail-under=100
--- src/main.rs
+++ src/main.rs
@@ -26,16 +26,6 @@ fn read_list_from_file(file_path: &str) -> Result<String, std::io::Error> {
     Ok(contents.trim().to_string())
 }
 
-fn sort_raw_card_list(cards: String) -> Vec<String> {
-    let mut card_list: Vec<String> = cards
-        .lines()
-        .filter(|line| !line.trim().is_empty())
-        .map(|line| line.trim().to_string())
-        .collect();
-    card_list.sort();
-    card_list
-}
-
 fn validate_card_list_entry(entry: &str) -> Result<(u32, String), String> {
     if let Some(caps) = CARD_NAME_PATTERN.captures(entry) {
         let count = caps
@@ -51,6 +41,17 @@ fn validate_card_list_entry(entry: &str) -> Result<(u32, String), String> {
     }
 }
 
+fn sort_card_list_entries(entries: &mut Vec<(u32, String)>) {
+    entries.sort_by(|a, b| {
+        let string_cmp = a.1.to_lowercase().cmp(&b.1.to_lowercase());
+        if std::cmp::Ordering::Equal == string_cmp {
+            a.0.cmp(&b.0)
+        } else {
+            string_cmp
+        }
+    });
+}
+
 fn validate_card_list(entries: &[&str]) -> Result<Vec<(u32, String)>, String> {
     let mut valid_entries = Vec::new();
     for entry in entries {
@@ -59,6 +60,7 @@ fn validate_card_list(entries: &[&str]) -> Result<Vec<(u32, String)>, String> {
             Err(err) => return Err(err),
         }
     }
+    sort_card_list_entries(&mut valid_entries);
     Ok(valid_entries)
 }
 
@@ -111,8 +113,10 @@ async fn build_decklist(
     raw_card_list: String,
     rate_milliseconds: u64,
 ) -> Result<Vec<(u32, Card)>, Error> {
-    let sorted_cards = sort_raw_card_list(raw_card_list);
-    let entries: Vec<&str> = sorted_cards.iter().map(|s| s.as_str()).collect();
+    let entries: Vec<&str> = raw_card_list
+        .lines()
+        .filter(|line| !line.trim().is_empty())
+        .collect();
     let valid_entries = validate_card_list(&entries).map_err(|err| Error::Other(err))?;
     find_cheapest_printing_of_list(valid_entries, rate_milliseconds).await
 }
@@ -156,210 +160,6 @@ mod tests {
     }
 
     #[test]
-    fn test_sort_raw_card_list() {
-        // https://moxfield.com/decks/K_R2ARDl_0W6Bs-mVi-vCA
-        let input = "1 Adarkar Wastes
-1 Alibou, Ancient Witness
-1 Ancient Den
-1 Angel of the Ruins
-1 Arcane Signet
-1 Astral Cornucopia
-1 Battlefield Forge
-1 Buried Ruin
-1 Cascade Bluffs
-1 Chain Reaction
-1 Chaos Warp
-1 Chrome Host Seedshark
-1 Clifftop Retreat
-1 Cloud Key
-1 Command Tower
-1 Coretapper
-1 Crystalline Crawler
-
-
-
-1 Moxite Refinery
-1 Mystic Monastery
-1 Organic Extinction
-1 Patrolling Peacemaker
-1 Pentad Prism
-1 Phyrexian Metamorph
-3 Plains
-1 Cyberdrive Awakener
-1 Darksteel Reactor
-1 Deepglow Skate
-1 Depthshaker Titan
-1 Dispatch
-1 Empowered Autogenerator
-1 Emry, Lurker of the Loch
-1 Enthusiastic Mechanaut
-1 Etched Oracle
-1 Etherium Sculptor
-1 Gavel of the Righteous
-1 Glacial Fortress
-1 Everflowing Chalice
-1 Evolving Wilds
-1 Exotic Orchard
-1 Experimental Augury
-1 Fumigate
-1 Glittering Massif
-1 Golem Foundry
-1 Great Furnace
-1 Hangarback Walker
-1 Insight Engine
-1 Irrigated Farmland
-3 Island
-1 Jhoira, Weatherlight Captain
-1 Kappa Cannoneer
-1 Karn's Bastion
-1 Kilo, Apogee Mind
-1 Lonely Sandbar
-1 Long-Range Sensor
-1 Lux Artillery
-1 Lux Cannon
-1 Mindless Automaton
-3 Mountain
-1 Pull from Tomorrow
-
-
-
-1 Radiant Summit
-1 Razortide Bridge
-1 Resourceful Defense
-1 Ripples of Potential
-1 Rugged Prairie
-1 Rustvale Bridge
-1 Seat of the Synod
-1 Secluded Steppe
-1 Shivan Reef
-1 Silverbluff Bridge
-1 Skycloud Expanse
-1 Sol Ring
-1 Solar Array
-1 Soul-Guide Lantern
-1 Spire of Industry
-1 Steel Overseer
-1 Sulfur Falls
-1 Surge Conductor
-1 Swan Song
-1 Swords to Plowshares
-1 Tekuthal, Inquiry Dominus
-1 Temple of Enlightenment
-1 Temple of Epiphany
-1 Temple of Triumph
-1 Tezzeret's Gambit
-1 The Mycosynth Gardens
-1 Thirst for Knowledge
-1 Thought Monitor
-1 Threefold Thunderhulk
-1 Thrummingbird
-1 Titan Forge
-1 Universal Surveillance
-1 Uthros Research Craft
-1 Wake the Past
-
-1 Inspirit, Flagship Vessel";
-        let expected = vec![
-            "1 Adarkar Wastes",
-            "1 Alibou, Ancient Witness",
-            "1 Ancient Den",
-            "1 Angel of the Ruins",
-            "1 Arcane Signet",
-            "1 Astral Cornucopia",
-            "1 Battlefield Forge",
-            "1 Buried Ruin",
-            "1 Cascade Bluffs",
-            "1 Chain Reaction",
-            "1 Chaos Warp",
-            "1 Chrome Host Seedshark",
-            "1 Clifftop Retreat",
-            "1 Cloud Key",
-            "1 Command Tower",
-            "1 Coretapper",
-            "1 Crystalline Crawler",
-            "1 Cyberdrive Awakener",
-            "1 Darksteel Reactor",
-            "1 Deepglow Skate",
-            "1 Depthshaker Titan",
-            "1 Dispatch",
-            "1 Empowered Autogenerator",
-            "1 Emry, Lurker of the Loch",
-            "1 Enthusiastic Mechanaut",
-            "1 Etched Oracle",
-            "1 Etherium Sculptor",
-            "1 Everflowing Chalice",
-            "1 Evolving Wilds",
-            "1 Exotic Orchard",
-            "1 Experimental Augury",
-            "1 Fumigate",
-            "1 Gavel of the Righteous",
-            "1 Glacial Fortress",
-            "1 Glittering Massif",
-            "1 Golem Foundry",
-            "1 Great Furnace",
-            "1 Hangarback Walker",
-            "1 Insight Engine",
-            "1 Inspirit, Flagship Vessel",
-            "1 Irrigated Farmland",
-            "1 Jhoira, Weatherlight Captain",
-            "1 Kappa Cannoneer",
-            "1 Karn's Bastion",
-            "1 Kilo, Apogee Mind",
-            "1 Lonely Sandbar",
-            "1 Long-Range Sensor",
-            "1 Lux Artillery",
-            "1 Lux Cannon",
-            "1 Mindless Automaton",
-            "1 Moxite Refinery",
-            "1 Mystic Monastery",
-            "1 Organic Extinction",
-            "1 Patrolling Peacemaker",
-            "1 Pentad Prism",
-            "1 Phyrexian Metamorph",
-            "1 Pull from Tomorrow",
-            "1 Radiant Summit",
-            "1 Razortide Bridge",
-            "1 Resourceful Defense",
-            "1 Ripples of Potential",
-            "1 Rugged Prairie",
-            "1 Rustvale Bridge",
-            "1 Seat of the Synod",
-            "1 Secluded Steppe",
-            "1 Shivan Reef",
-            "1 Silverbluff Bridge",
-            "1 Skycloud Expanse",
-            "1 Sol Ring",
-            "1 Solar Array",
-            "1 Soul-Guide Lantern",
-            "1 Spire of Industry",
-            "1 Steel Overseer",
-            "1 Sulfur Falls",
-            "1 Surge Conductor",
-            "1 Swan Song",
-            "1 Swords to Plowshares",
-            "1 Tekuthal, Inquiry Dominus",
-            "1 Temple of Enlightenment",
-            "1 Temple of Epiphany",
-            "1 Temple of Triumph",
-            "1 Tezzeret's Gambit",
-            "1 The Mycosynth Gardens",
-            "1 Thirst for Knowledge",
-            "1 Thought Monitor",
-            "1 Threefold Thunderhulk",
-            "1 Thrummingbird",
-            "1 Titan Forge",
-            "1 Universal Surveillance",
-            "1 Uthros Research Craft",
-            "1 Wake the Past",
-            "3 Island",
-            "3 Mountain",
-            "3 Plains",
-        ];
-        let sorted = sort_raw_card_list(input.to_string());
-        assert_eq!(sorted, expected);
-    }
-
-    #[test]
     fn test_validate_card_list_entry() {
         let entry = "3 Mountain";
         let result = validate_card_list_entry(entry);
@@ -394,16 +194,37 @@ mod tests {
     }
 
     #[test]
+    fn test_sort_card_list_entries() {
+        let mut entries = vec![
+            (2, "Island".to_string()),
+            (1, "Plains".to_string()),
+            (3, "Mountain".to_string()),
+            (2, "Forest".to_string()),
+        ];
+        sort_card_list_entries(&mut entries);
+        assert_eq!(entries.len(), 4);
+        assert_eq!(entries[0].0, 2);
+        assert_eq!(entries[0].1, "Forest");
+        assert_eq!(entries[1].0, 2);
+        assert_eq!(entries[1].1, "Island");
+        assert_eq!(entries[2].0, 3);
+        assert_eq!(entries[2].1, "Mountain");
+        assert_eq!(entries[3].0, 1);
+        assert_eq!(entries[3].1, "Plains");
+    }
+
+    #[test]
     fn test_validate_card_list() {
         let entries = vec!["3 Mountain", "2 Island", "1 Plains"];
         let result = validate_card_list(&entries);
+        print!("Validating card list: {:?}", result);
         assert!(result.is_ok());
         let valid_entries = result.unwrap();
         assert_eq!(valid_entries.len(), 3);
-        assert_eq!(valid_entries[0].0, 3);
-        assert_eq!(valid_entries[0].1, "Mountain");
-        assert_eq!(valid_entries[1].0, 2);
-        assert_eq!(valid_entries[1].1, "Island");
+        assert_eq!(valid_entries[0].0, 2);
+        assert_eq!(valid_entries[0].1, "Island");
+        assert_eq!(valid_entries[1].0, 3);
+        assert_eq!(valid_entries[1].1, "Mountain");
         assert_eq!(valid_entries[2].0, 1);
         assert_eq!(valid_entries[2].1, "Plains");
 
